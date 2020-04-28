20dfb29b0f2756bb3748ceaf071dbd62df1a6e22
Ihor Vodko <54189613+IhorVodko@users.noreply.github.com>
2020-04-28 13:36:23 -0400

    Update and rename Python If-Else to Python If-Else.py

--- Python/Python If-Else.py
+++ Python/Python If-Else.py
@@ -3,7 +3,6 @@
 #Level: Easy
 
 
-```python
 if n%2 !=0:
     print('Weird')
 elif n%2 == 0 and n in range(2, 6):
@@ -12,4 +11,3 @@ elif n%2 == 0 and n in range(6, 21):
     print('Weird')
 elif n%2 == 0 and n > 20:
     print('Not Weird')
-```
