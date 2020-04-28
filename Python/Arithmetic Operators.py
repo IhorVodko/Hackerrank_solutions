1c52796711dc791b659aecaa7a48f16ce16fc09a
Ihor Vodko <54189613+IhorVodko@users.noreply.github.com>
2020-04-28 13:39:25 -0400

    Create Arithmetic Operators.py

---
+++ Python/Arithmetic Operators.py
@@ -0,0 +1,10 @@
+#Problem: https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+#Score: 10
+#Level: Easy
+
+
+a = int(input())
+b = int(input())
+print(a+b)
+print(a-b)
+print(a*b)
