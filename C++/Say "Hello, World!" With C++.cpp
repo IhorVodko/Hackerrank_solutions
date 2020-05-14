5ff78d6eee243cc4a8055408623093331cc9ad28
Ihor Vodko <54189613+IhorVodko@users.noreply.github.com>
2020-05-13 20:16:37 -0400

    Create Say "Hello, World!" With C++.cpp

---
+++ C++/Say "Hello, World!" With C++.cpp
@@ -0,0 +1,12 @@
+//Problem: https://www.hackerrank.com/challenges/cpp-hello-world/problem
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Hello, World!" << endl;
+
+    return 0;
+}
