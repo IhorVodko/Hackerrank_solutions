d8e218836f70ba1db5771f0f0a8d3902ae0dc3b9
Ihor Vodko <54189613+IhorVodko@users.noreply.github.com>
2020-05-13 20:27:35 -0400

    Update Input and Output.cpp

--- C++/Input and Output.cpp
+++ C++/Input and Output.cpp
@@ -1,1 +1,17 @@
 //Problem: https://www.hackerrank.com/challenges/cpp-input-and-output/problem?h_r=next-challenge&h_v=zen
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int i {0};
+    int sum {0};
+
+    while (cin >> i) {
+        sum += i;
+    }
+    cout << sum << endl;
+    return 0;
+}
