9c45a922956a6cd98a8da743832bedb745d63add
Ihor Vodko <54189613+IhorVodko@users.noreply.github.com>
2020-05-14 20:42:20 -0400

    Update Type of Triangle.sql

--- SQL/Type of Triangle.sql
+++ SQL/Type of Triangle.sql
@@ -1,4 +1,11 @@
 --Problem: https://www.hackerrank.com/challenges/what-type-of-triangle/problem
 
 
-
+SELECT 
+CASE
+    WHEN A = B AND A = C THEN "Equilateral"
+    WHEN A >= (B + C) OR B >= (A + C) OR C >= (A + B) THEN "Not A Triangle"
+    WHEN A = B OR A = C OR B = C THEN "Isosceles"
+    ELSE "Scalene"
+END
+FROM TRIANGLES;
