40b1cefbea293d1dc1331661d78476be6b8e22fe
Ihor Vodko <54189613+IhorVodko@users.noreply.github.com>
2020-05-16 23:35:13 -0400

    Create Structs.cpp

---
+++ C++/Structs.cpp
@@ -0,0 +1,11 @@
+//Problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+
+
+#include <string>
+
+struct Student {
+    int age{};
+    std::string first_name;
+    std::string last_name;
+    int standard;
+};
