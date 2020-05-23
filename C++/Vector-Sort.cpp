c5dd871aca7c8aed1fc29f56cfc0c77a3f937a6a
Ihor Vodko <54189613+IhorVodko@users.noreply.github.com>
2020-05-23 00:06:38 -0400

    Update Vector-Sort.cpp

--- C++/Vector-Sort.cpp
+++ C++/Vector-Sort.cpp
@@ -1,1 +1,24 @@
 #Problem: https://www.hackerrank.com/challenges/vector-sort/problem
+
+
+#include <vector>
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+    vector<int> v;
+    size_t t{};
+    int i, j{};
+
+    cin >> t;
+    while (cin >> i) {v.push_back(i); }
+  
+    sort(v.begin(), v.end());
+  
+    for (const auto &num: v)
+        cout << num << ' ';
+ 
+    return 0;
+}
