d6956c56dfa5e3a4ae6f06a6ad3f28489a2d6bec
Ihor Vodko <54189613+IhorVodko@users.noreply.github.com>
2020-05-24 22:21:03 -0400

    Update Vector-Erase.cpp

--- C++/Vector-Erase.cpp
+++ C++/Vector-Erase.cpp
@@ -1,2 +1,38 @@
 //Problem: https://www.hackerrank.com/challenges/vector-erase/problem
 
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+
+    vector<int> v;
+    size_t t;
+    int temp{}, b{}, e{};
+
+    cin >> t;
+
+    for (size_t i = 0; i < t; i++) {
+        int num;
+        cin >> num;
+        v.push_back(num);
+    }
+
+    cin >> b;
+    v.erase(begin(v) + b - 1);
+
+    cin >> b >> e;
+    v.erase(begin(v) + b - 1, begin(v) + e - 1);
+
+    cout << v.size() << endl;
+
+    for (auto num: v)
+        cout << num << ' ';
+    cout << endl;
+
+    return 0;
+}
