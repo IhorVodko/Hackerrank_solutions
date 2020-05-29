e8566854ab82e743cbe88369f54455bccc3b7a53
Ihor Vodko <ihorfinance@gmail.com>
2020-05-29 21:44:26 +0300

    Update Top Earners.sql

--- SQL/Top Earners.sql
+++ SQL/Top Earners.sql
@@ -1,2 +1,8 @@
 --Problem: https://github.com/IhorVodko/Hackerrank_solutions/edit/master/SQL/Top%20Earners.sql
 
+
+SELECT (months * salary) as earnings, COUNT(*)
+FROM EMPLOYEE
+GROUP BY earnings
+ORDER BY earnings DESC
+LIMIT 1;
