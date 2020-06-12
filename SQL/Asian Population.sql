d702cb82a63f7ed38a9bd5f39224a37e067b6757
Ihor Vodko <ihorfinance@gmail.com>
2020-06-12 22:54:25 +0300

    Update Asian Population.sql

--- SQL/Asian Population.sql
+++ SQL/Asian Population.sql
@@ -1,3 +1,8 @@
 --Problem https://github.com/IhorVodko/Hackerrank_solutions/edit/master/SQL/Asian%20Population.sql
 
 
+SELECT SUM(CI.population)
+FROM CITY as CI
+JOIN COUNTRY as CO
+ON CI.countrycode = CO.code
+WHERE CO.continent = "Asia";
