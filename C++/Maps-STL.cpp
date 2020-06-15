969410d113414b07056e3b3a904dd17a3e5ec2a2
Ihor Vodko <ihorfinance@gmail.com>
2020-06-15 15:13:42 +0300

    Update Maps-STL.cpp

--- C++/Maps-STL.cpp
+++ C++/Maps-STL.cpp
@@ -1,3 +1,34 @@
 //Problem: https://github.com/IhorVodko/Hackerrank_solutions/edit/master/C++/Maps-STL.cpp
 
 
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <string>
+
+
+int main() {
+    
+    std::map<std::string, int> m;
+    size_t q;
+    std::cin >> q;
+    int type, mark;
+    std::string name;
+
+    for (size_t i = 0; i < q; ++i) {
+        std::cin >> type >> name;
+        switch(type) {
+            case 1:
+                std::cin >> mark;
+                m[name] += mark;
+                break;                
+            case 2: 
+                m.erase(name);
+                break;
+            case 3:
+                std::cout << m[name] << std::endl; 
+                break;
+        }
+    }
+
+}
