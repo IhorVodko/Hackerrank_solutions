9079d07b78052740177539af5c57256001b1fc8c
Ihor Vodko <ihorfinance@gmail.com>
2020-06-16 14:24:03 +0300

    Update Print Pretty.cpp

--- C++/Print Pretty.cpp
+++ C++/Print Pretty.cpp
@@ -1,3 +1,28 @@
 //Problem: https://www.hackerrank.com/challenges/prettyprint/problem
 
 
+#include <iostream>
+#include <iomanip> 
+using namespace std;
+
+int main() {
+	int T; cin >> T;
+	cout << setiosflags(ios::uppercase);
+	cout << setw(0xf) << internal;
+	while(T--) {
+		double A; cin >> A;
+		double B; cin >> B;
+		double C; cin >> C;
+
+        std::cout << std::hex << std::showbase << std::left << std::nouppercase;
+        std::cout << (long long) A << std::endl;
+
+        std::cout << std::dec << std::setw(15) << std::right << std::setfill('_') << std::showpos << std::fixed << std::setprecision(2);
+        std::cout << B << std::endl; 
+
+        std::cout << std::scientific << std::setprecision(9) << std::uppercase << std::noshowpos;
+        std::cout << C << std::endl;
+	}
+	return 0;
+
+}
