0e11fb0894a1650708a216871fd0204e44683e0f
Ihor Vodko <ihorfinance@gmail.com>
2020-06-16 15:15:06 +0300

    Update Inheritance Introduction.cpp

--- C++/Inheritance Introduction.cpp
+++ C++/Inheritance Introduction.cpp
@@ -1,1 +1,35 @@
-//Problem: 
+//Problem:  https://www.hackerrank.com/challenges/inheritance-introduction/problem
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+class Triangle{
+    public:
+    	void triangle(){
+     		cout<<"I am a triangle\n";
+    	}
+};
+
+class Isosceles : public Triangle{
+    public:
+    	void isosceles(){
+    		cout<<"I am an isosceles triangle\n";
+    	}
+  		void description() {
+              std::cout << "In an isosceles triangle two sides are equal" << std::endl; 
+          }
+};
+
+int main(){
+    Isosceles isc;
+    isc.isosceles();
+  	isc.description();
+    isc.triangle();
+    return 0;
+}
