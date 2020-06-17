89550c3666cdbb4c4b5de0b82e5e08ecae645779
Ihor Vodko <ihorfinance@gmail.com>
2020-06-17 17:30:13 +0300

    Update Preprocessor Solution.cpp

--- C++/Preprocessor Solution.cpp
+++ C++/Preprocessor Solution.cpp
@@ -1,3 +1,37 @@
 //Problem: https://www.hackerrank.com/challenges/preprocessor-solution/problem
 
 
+#define toStr(x) #x
+#define io(v) std::cin >> (v)
+#define FUNCTION(name, op) void name(int &a, int b) {if (b op a) a = b;}
+#define INF 100
+#define foreach(v, i) for(int i = 0; i < v.size(); ++i)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#if !defined toStr || !defined io || !defined FUNCTION || !defined INF
+#error Missing preprocessor definitions
+#endif 
+
+FUNCTION(minimum, <)
+FUNCTION(maximum, >)
+
+int main(){
+	int n; cin >> n;
+	vector<int> v(n);
+	foreach(v, i) {
+		io(v)[i];
+	}
+	int mn = INF;
+	int mx = -INF;
+	foreach(v, i) {
+		minimum(mn, v[i]);
+		maximum(mx, v[i]);
+	}
+	int ans = mx - mn;
+	cout << toStr(Result =) <<' '<< ans;
+	return 0;
+
+}
