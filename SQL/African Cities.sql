d0a128435dc9a388f318b44ba06e894b2669f556
Ihor Vodko <ihorfinance@gmail.com>
2020-06-20 23:45:18 +0300

    Update African Cities.sql

--- SQL/African Cities.sql
+++ SQL/African Cities.sql
@@ -1,1 +1,8 @@
 --Problem: https://www.hackerrank.com/challenges/african-cities/problem
+
+
+SELECT CI.name
+FROM CITY AS CI
+JOIN COUNTRY AS CO
+ON CI.countrycode = CO.code
+WHERE CO.continent = 'Africa';
