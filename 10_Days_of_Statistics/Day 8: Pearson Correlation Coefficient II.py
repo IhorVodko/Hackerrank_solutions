f3541e4d86053b1022643975939aa5c7c60e2624
Ihor Vodko <ihorfinance@gmail.com>
2020-06-25 20:08:49 +0300

    Create Day 8: Pearson Correlation Coefficient II.py

---
+++ 10_Days_of_Statistics/Day 8: Pearson Correlation Coefficient II.py
@@ -0,0 +1,11 @@
+#Problem: https://www.hackerrank.com/challenges/s10-mcq-7/problem
+
+
+#3*x + 4*y + 8 = 0
+#4*x + 3*y +7 = 0
+
+b1 = -3/4
+b2 = -3/4
+
+r = -(b1*b2)**0.5   # original slope from equations is negative
+r 
