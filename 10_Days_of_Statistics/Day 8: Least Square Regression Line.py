939fda18c3acf132387b138c89f687bb1d642642
Ihor Vodko <ihorfinance@gmail.com>
2020-06-25 19:37:27 +0300

    Update Day 8: Least Square Regression Line.py

--- 10_Days_of_Statistics/Day 8: Least Square Regression Line.py
+++ 10_Days_of_Statistics/Day 8: Least Square Regression Line.py
@@ -1,1 +1,12 @@
+#problem: https://www.hackerrank.com/challenges/s10-least-square-regression-line/problem
 
+
+n = 5
+X = [95, 85, 80, 70, 60]
+Y = [85, 95, 70, 65, 70]
+
+b = (n*sum([X[i]*Y[i] for i in range(n)]) - sum(X)*sum(Y)) / \
+    (n*sum([X[i]**2 for i in range(n)]) - sum(X)**2)
+a = sum(Y)/n - b*(sum(X)/n)
+
+print(round(a + b*80, 3))
