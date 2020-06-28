c5e3778c9772137b92d1dae1007c3cdd97cd7e33
Ihor Vodko <ihorfinance@gmail.com>
2020-06-28 22:17:43 +0300

    Update Average Population of Each Continent.sql

--- SQL/Average Population of Each Continent.sql
+++ SQL/Average Population of Each Continent.sql
@@ -1,3 +1,8 @@
 --Problem: https://www.hackerrank.com/challenges/average-population-of-each-continent/problem
 
 
+SELECT  CO.continent, FLOOR(AVG(CI.population))
+FROM CITY AS CI
+JOIN COUNTRY AS CO
+ON CI.countryCode = CO.code
+GROUP BY CO.continent;
