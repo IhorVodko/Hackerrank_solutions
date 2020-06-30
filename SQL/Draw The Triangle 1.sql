089c881cf9d907b55b01415fb172543a6d324827
Ihor Vodko <ihorfinance@gmail.com>
2020-06-30 21:25:47 +0300

    Update Draw The Triangle 1.sql

--- SQL/Draw The Triangle 1.sql
+++ SQL/Draw The Triangle 1.sql
@@ -1,1 +1,7 @@
+--Problem: https://github.com/IhorVodko/Hackerrank_solutions/edit/master/SQL/Draw%20The%20Triangle%201.sql
 
+
+SET @number = 21;
+SELECT REPEAT('* ', @number := @number - 1) 
+FROM information_schema.tables 
+WHERE @number > 0;
