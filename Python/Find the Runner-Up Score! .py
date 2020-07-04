e410e22a371c5264a2bfa6772c0b9e31541d7d83
Ihor Vodko <ihorfinance@gmail.com>
2020-07-04 23:09:20 +0300

    Update Find the Runner-Up Score! .py

--- Python/Find the Runner-Up Score! .py
+++ Python/Find the Runner-Up Score! .py
@@ -1,1 +1,9 @@
 #Problem: https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem
+
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+
+dict_ = {}
+print( sorted(list(set(arr)))[-2] )
