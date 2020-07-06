b9b88d5d73334027107da62e55523a13c544df69
Ihor Vodko <ihorfinance@gmail.com>
2020-07-06 21:27:12 +0300

    Update Nested Lists.py

--- Python/Nested Lists.py
+++ Python/Nested Lists.py
@@ -1,1 +1,12 @@
 #Problem: https://www.hackerrank.com/challenges/nested-list/problem
+
+
+if __name__ == '__main__':
+    list_ = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        list_.append([name, score])
+
+second_lowest  = sorted(list(set([x[1] for x in list_])))[1]
+print('\n'.join([x[0] for x in sorted(list_) if x[1] == second_lowest]))
