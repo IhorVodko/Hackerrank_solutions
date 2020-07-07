86ca4ed38f803233a99842d719972e1994c5bc2b
Ihor Vodko <ihorfinance@gmail.com>
2020-07-07 21:56:58 +0300

    Update Finding the percentage.py

--- Python/Finding the percentage.py
+++ Python/Finding the percentage.py
@@ -1,1 +1,14 @@
 #Problem: https://www.hackerrank.com/challenges/finding-the-percentage/problem
+
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+
+avg = sum(student_marks[query_name]) / 3
+print(f'{avg:.2f}')
