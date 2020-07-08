606ab270f6dceba483da81c6971b3d8ab6647318
Ihor Vodko <ihorfinance@gmail.com>
2020-07-08 23:19:38 +0300

    Update Lists.py

--- Python/Lists.py
+++ Python/Lists.py
@@ -1,1 +1,16 @@
 #Problem: https://www.hackerrank.com/challenges/python-lists/problem
+
+
+n = int(input())
+list_ = []
+for _ in range(n):
+    line = input().split()
+    cmd = line[0]
+    args = line[1:]
+    if cmd != "print":
+        cmd += "("+ ",".join(args) +")"
+        eval("list_." + cmd)
+    else:
+        print(list_)
+
+
