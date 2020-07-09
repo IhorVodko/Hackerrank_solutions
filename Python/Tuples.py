310f11abb45116e6fcbb1cd620311286d9a87014
Ihor Vodko <ihorfinance@gmail.com>
2020-07-09 21:35:51 +0300

    Update Tuples.py

--- Python/Tuples.py
+++ Python/Tuples.py
@@ -1,1 +1,9 @@
 #Problem: https://www.hackerrank.com/challenges/python-tuples/problem
+
+
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+
+result = hash(tuple(integer_list))
+print(result)
