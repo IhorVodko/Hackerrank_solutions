063118b980e58c76252f964238a02182973542fc
Ihor Vodko <ihorfinance@gmail.com>
2020-07-11 19:02:30 +0300

    Update sWAP cASE.py

--- Python/sWAP cASE.py
+++ Python/sWAP cASE.py
@@ -1,1 +1,8 @@
 #Problem: https://www.hackerrank.com/challenges/swap-case/problem
+
+
+def swap_case(s):
+
+    solution = ''.join([char.lower() if char.isupper() else char.upper() for char in s])
+    
+    return solution
