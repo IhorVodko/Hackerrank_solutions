922e0beb394c5e6ddb35629b9a876abe4cc19ceb
Ihor Vodko <ihorfinance@gmail.com>
2020-07-11 18:59:57 +0300

    Update Save the Prisoner!.cpp

--- Algorithms/Save the Prisoner!.cpp
+++ Algorithms/Save the Prisoner!.cpp
@@ -1,1 +1,81 @@
-#Problem: 
+#Problem: https://www.hackerrank.com/challenges/save-the-prisoner/problem
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the saveThePrisoner function below.
+int saveThePrisoner(int n, int m, int s) {
+
+    int reminder = 0;
+    reminder = m%n;
+    int seat_no_bound = 0;
+    seat_no_bound = (s+(reminder-1));
+    int seat = 0;
+    seat = seat_no_bound%n;
+    
+    return seat == 0 ? n : seat;
+    
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string nms_temp;
+        getline(cin, nms_temp);
+
+        vector<string> nms = split_string(nms_temp);
+
+        int n = stoi(nms[0]);
+
+        int m = stoi(nms[1]);
+
+        int s = stoi(nms[2]);
+
+        int result = saveThePrisoner(n, m, s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
