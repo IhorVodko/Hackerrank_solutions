a02266ced8688f86e0344258f00418c8a0b31876
Ihor Vodko <ihorfinance@gmail.com>
2020-07-12 16:22:05 +0300

    Update String Split and Join.py

--- Python/String Split and Join.py
+++ Python/String Split and Join.py
@@ -1,1 +1,16 @@
+#Problem: https://www.hackerrank.com/challenges/python-string-split-and-join/problem
 
+
+def split_and_join(line):
+  
+    # write your code here
+    line_splitted = line.split(" ")
+    line_joined = "-".join(line_splitted)
+
+    return line_joined
+
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
