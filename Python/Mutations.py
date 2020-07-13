9d8ec93c8a04fea86dc8c918527551e845318d2e
Ihor Vodko <ihorfinance@gmail.com>
2020-07-13 19:53:30 +0300

    Update Mutations.py

--- Python/Mutations.py
+++ Python/Mutations.py
@@ -1,1 +1,15 @@
+#Problem: https://www.hackerrank.com/challenges/python-mutations/problem
 
+
+def mutate_string(string, position, character):
+
+    string = string[:position] + character + string[(position+1):]
+
+    return string
+
+  
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
