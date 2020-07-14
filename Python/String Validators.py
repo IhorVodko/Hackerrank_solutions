d071e03be2abb92df8bb08275ef7716dad29cd42
Ihor Vodko <ihorfinance@gmail.com>
2020-07-14 20:21:24 +0300

    Update String Validators.py

--- Python/String Validators.py
+++ Python/String Validators.py
@@ -1,1 +1,8 @@
+#Problem: https://www.hackerrank.com/challenges/string-validators/problem
 
+
+if __name__ == '__main__':
+    s = input()
+
+for check in ['isalnum', 'isalpha', 'isdigit', 'islower', 'isupper']:
+    print( any([eval('char.' + check + '()') for char in s]) ) 
