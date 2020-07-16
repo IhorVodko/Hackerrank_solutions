7cecc10544e644a95fdaffa2b8063ee86c93ee86
Ihor Vodko <ihorfinance@gmail.com>
2020-07-16 23:03:15 +0300

    Create Text Wrap.py

---
+++ Python/Text Wrap.py
@@ -0,0 +1,18 @@
+#Problem: https://www.hackerrank.com/challenges/text-wrap/problem
+
+
+import textwrap
+
+
+def wrap(string, max_width):
+
+    string_splitted = [string[i : i+max_width] for i \
+                        in range(0, len(string), max_width)]
+                 
+    return '\n'.join(string_splitted)
+
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
