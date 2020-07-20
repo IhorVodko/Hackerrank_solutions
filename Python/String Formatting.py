383c3dc9ea2a822d85f2a766377321adc740249b
Ihor Vodko <ihorfinance@gmail.com>
2020-07-20 21:36:00 +0300

    Update String Formatting.py

--- Python/String Formatting.py
+++ Python/String Formatting.py
@@ -1,1 +1,17 @@
+#Problem: https://www.hackerrank.com/challenges/python-string-formatting/problem
 
+
+def print_formatted(number):
+    # your code goes here
+    length = len(bin(number)[2:])
+    for num in range(1, number+1):
+        dec_ = str(num)
+        oct_ = oct(num)[2:]
+        hex_ = hex(num).upper()[2:]
+        bin_ = bin(num)[2:]
+        print(' '.join(map(lambda x: x.rjust(length), [dec_, oct_, hex_, bin_])) )
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
