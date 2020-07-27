db8d01ad0de403db22c6c3148ba328c5affa6ba2
Ihor Vodko <ihorfinance@gmail.com>
2020-07-28 01:28:28 +0300

    Update collections.Counter().py

--- Python/collections.Counter().py
+++ Python/collections.Counter().py
@@ -1,1 +1,28 @@
 #Problem: https://www.hackerrank.com/challenges/collections-counter/problem
+
+
+def revenue(sizes, size_price):
+
+    sizes_count = Counter(sizes)
+    revenue = 0
+    for item in size_price:
+        if sizes_count.get(item[0], 0) != 0:
+            revenue += item[1]
+            sizes_count.update({item[0]: -1})
+
+    print(revenue)
+
+    
+if __name__ == '__main__':
+
+    from collections import Counter
+
+    num_sizes = int(input().strip())
+    sizes = [*map(int, input().strip().split())]
+    num_cast = int(input().strip())
+    size_price = []
+    for _ in range(num_cast):
+        size_price.append([*map(int, input().strip().split())])
+    
+    revenue(sizes, size_price)
+
