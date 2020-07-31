5a508bef541106326b95572aca0d5fb4021f4488
Ihor Vodko <ihorfinance@gmail.com>
2020-07-31 21:54:27 +0300

    Update itertools.permutations().py

--- Python/itertools.permutations().py
+++ Python/itertools.permutations().py
@@ -1,1 +1,9 @@
 #Problme: https://github.com/IhorVodko/Hackerrank_solutions/new/master/Python
+
+
+from itertools import permutations
+
+string, length = input().strip().split()
+solution = [''.join(perm) for perm in permutations(sorted(string), int(length))]
+
+print(*solution, sep = '\n')
