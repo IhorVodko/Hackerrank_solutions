754013aa2ef176f854e838a7f4bbc6d174457f2e
Ihor Vodko <ihorfinance@gmail.com>
2020-08-03 23:48:06 +0300

    Update Calendar Module.py

--- Python/Calendar Module.py
+++ Python/Calendar Module.py
@@ -1,1 +1,8 @@
+#Problem: https://www.hackerrank.com/challenges/calendar-module/problem
 
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import calendar
+
+m, d, y  = map(int, input().strip().split())
+print([*calendar.day_name][calendar.weekday(y, m, d)].upper())
