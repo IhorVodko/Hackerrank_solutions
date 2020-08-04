5885e8b388e10f090672f1170827f76f85562743
Ihor Vodko <ihorfinance@gmail.com>
2020-08-04 23:47:52 +0300

    Update Exceptions.py

--- Python/Exceptions.py
+++ Python/Exceptions.py
@@ -1,1 +1,13 @@
+#Problem: https://github.com/IhorVodko/Hackerrank_solutions/edit/master/Python/Exceptions.py
 
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input().strip())
+
+for _ in range(n):
+    try:
+        num1, num2 = map(int, input().split())
+        print(num1//num2)
+    except BaseException as e:
+        print(f'Error Code: {e}')
+    
