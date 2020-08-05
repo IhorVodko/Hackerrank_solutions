da3aae25b38b7a5cb179bf555c864aca37c14a6e
Ihor Vodko <ihorfinance@gmail.com>
2020-08-05 17:05:19 +0300

    Create Matching Word & Non-Word Character.py

---
+++ Regex/Matching Word & Non-Word Character.py
@@ -0,0 +1,8 @@
+#Problem: https://www.hackerrank.com/challenges/matching-word-non-word/problem
+
+
+Regex_Pattern = r'\w{3}\W\w{10}\W\w{3}'	# Do not delete 'r'.
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
