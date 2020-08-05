b14870ea7ff8fbc4f5d795c5bdeaa7ab6ccb5653
Ihor Vodko <ihorfinance@gmail.com>
2020-08-05 17:35:39 +0300

    Create Matching Character Ranges.py

---
+++ Regex/Matching Character Ranges.py
@@ -0,0 +1,8 @@
+#Problem: https://www.hackerrank.com/challenges/matching-range-of-characters/problem
+
+
+Regex_Pattern = r'^[a-z][1-9][^a-z][^A-Z][A-Z]'	# Do not delete 'r'.
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
