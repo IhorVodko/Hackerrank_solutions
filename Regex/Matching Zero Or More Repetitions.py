d6bd7dcb73d46d286c0217a0cf00425135a25e65
Ihor Vodko <ihorfinance@gmail.com>
2020-08-05 18:17:00 +0300

    Create Matching Zero Or More Repetitions.py

---
+++ Regex/Matching Zero Or More Repetitions.py
@@ -0,0 +1,8 @@
+#Problem: https://www.hackerrank.com/challenges/matching-zero-or-more-repetitions/problem
+
+
+Regex_Pattern = r'^\d{2,}[a-z]*[A-Z]*$'	# Do not delete 'r'.
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
