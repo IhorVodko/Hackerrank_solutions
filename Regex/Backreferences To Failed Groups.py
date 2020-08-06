1a445e5916ee743370cb19b8f60c68ce37526431
Ihor Vodko <ihorfinance@gmail.com>
2020-08-06 21:17:01 +0300

    Create Backreferences To Failed Groups.py

---
+++ Regex/Backreferences To Failed Groups.py
@@ -0,0 +1,8 @@
+#Problem: https://www.hackerrank.com/challenges/backreferences-to-failed-groups/problem
+
+
+Regex_Pattern = r'^\d{2}(-?)(\d{2}\1){2}\d{2}$'	# Do not delete 'r'.
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
