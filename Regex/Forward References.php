3190aa956df1922ba3c6701bf17b771ca844337a
Ihor Vodko <ihorfinance@gmail.com>
2020-08-10 23:49:04 +0300

    Create Forward References.php

---
+++ Regex/Forward References.php
@@ -0,0 +1,17 @@
+#Problem: https://www.hackerrank.com/challenges/forward-references/problem
+
+
+<?php
+
+$Regex_Pattern = '/^(\2tic|(tac))+$/'; //Do not delete '/'. Replace __________ with your regex.
+
+$handle = fopen ("php://stdin","r");
+$Test_String = fgets($handle);
+if(preg_match($Regex_Pattern, $Test_String, $output_array)){
+    print ("true");
+} else {
+    print ("false");
+}
+
+fclose($handle);
+?>
