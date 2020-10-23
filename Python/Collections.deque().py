9920fd0c69acd15e89d7704740dc54f871f5df5a
Ihor Vodko <ihorfinance@gmail.com>
2020-10-23 19:00:16 +0300

    Update Collections.deque().py

--- Python/Collections.deque().py
+++ Python/Collections.deque().py
@@ -1,1 +1,11 @@
-#Problem
+#Problem: https://www.hackerrank.com/challenges/py-collections-deque/problem
+
+
+from collections import deque
+
+d = deque()
+for _ in range(int(input())):
+    operation = input().split() + ['']
+    eval(f'd.{operation[0]}({operation[1]})')
+
+print(*d)
