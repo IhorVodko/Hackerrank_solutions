56b909adc227e4eadf227bb7f8b96c510db85a3f
Ihor Vodko <ihorfinance@gmail.com>
2020-11-22 14:26:36 +0200

    Create Validating Roman Numerals.py

---
+++ Python/Validating Roman Numerals.py
@@ -0,0 +1,15 @@
+#Problem: https://www.hackerrank.com/challenges/validate-a-roman-number/problem
+
+
+import re
+
+thousand = 'M{0,3}'
+hundred = '(C[MD]|D?C{0,3})'
+ten = '(X[CL]|L?X{0,3})'
+digit = '(I[VX]|V?I{0,3})'
+
+regex_pattern = re.compile('^' + thousand + hundred+ten+digit +'$')	# Do not delete 'r'
+
+
+import re
+print(str(bool(re.match(regex_pattern, input()))))
