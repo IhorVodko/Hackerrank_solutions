802cb73ad061ef2bc53c439023cbd4cbfab8dee2
Ihor Vodko <ihorfinance@gmail.com>
2020-12-30 13:08:11 +0200

    Create Arrays.py

---
+++ Python/Arrays.py
@@ -0,0 +1,15 @@
+#problem: https://www.hackerrank.com/challenges/np-arrays/problem
+
+
+import numpy
+
+def arrays(arr):
+    # complete this function
+    # use numpy.array
+    
+    return numpy.array(arr[::-1], float)
+    
+
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
