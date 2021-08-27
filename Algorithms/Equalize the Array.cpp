14c0689bb24d067d13556a900390ef6c1fcb2ea4
Ihor Vodko <ihorfinance@gmail.com>
2021-08-27 20:58:19 +0300

    Create Equalize the Array.cpp

---
+++ Algorithms/Equalize the Array.cpp
@@ -0,0 +1,100 @@
+#Problem: https://www.hackerrank.com/challenges/equality-in-a-array/problem
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'equalizeArray' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+int equalizeArray(const vector<int>& arr) 
+{
+    auto sz = arr.size();
+    map<int, int> count;
+    for (auto itB = arr.cbegin(); itB != arr.cend(); ++itB)
+    {
+        count[*itB]++;
+    }
+    auto ret = max_element(count.cbegin(), count.cend(), 
+        [](const auto& p1, const auto& p2){return p1.second < p2.second;});
+    
+    return (sz - ret->second);        
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int result = equalizeArray(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
