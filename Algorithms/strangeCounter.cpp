3888d8ad030f4736f313fc74490ca5bd6b85b460
Ihor Vodko <ihorfinance@gmail.com>
2024-02-02 22:05:39 +0200

    Create strangeCounter.cpp

---
+++ Algorithms/strangeCounter.cpp
@@ -0,0 +1,67 @@
+//problem https://www.hackerrank.com/challenges/strange-code/problem?isFullScreen=true
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'strangeCounter' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER t as parameter.
+ */
+
+long strangeCounter(long t) {
+    long int 
+            time = 1
+        ,   step = 3;
+    while (time <= t)
+    {
+        time = step + time;
+        step = step * 2;
+    }
+    return time - t;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    long t = stol(ltrim(rtrim(t_temp)));
+
+    long result = strangeCounter(t);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
