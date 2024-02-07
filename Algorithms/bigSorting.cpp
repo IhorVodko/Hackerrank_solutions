5a3c40d21f22e99d1d7b4426a729bdd07aa9aeae
Ihor Vodko <ihorfinance@gmail.com>
2024-02-07 23:12:40 +0200

    Create bigSorting.cpp

---
+++ Algorithms/bigSorting.cpp
@@ -0,0 +1,91 @@
+// problem : https://www.hackerrank.com/challenges/big-sorting/problem?isFullScreen=true
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'bigSorting' function below.
+ *
+ * The function is expected to return a STRING_ARRAY.
+ * The function accepts STRING_ARRAY unsorted as parameter.
+ */
+
+// include <string>
+// include <algorithm>
+std::vector<std::string> bigSorting(std::vector<std::string> unsorted) {
+    std::sort(
+            std::begin(unsorted)
+        ,   std::end(unsorted)
+        ,   [](
+                    std::string const & _lhs
+                ,   std::string const & _rhs ){
+                    return _lhs.size() == _rhs.size()
+                        ? _lhs < _rhs
+                        : _lhs.size() < _rhs.size()
+                    ;
+            }
+    );
+    return unsorted;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    vector<string> unsorted(n);
+
+    for (int i = 0; i < n; i++) {
+        string unsorted_item;
+        getline(cin, unsorted_item);
+
+        unsorted[i] = unsorted_item;
+    }
+
+    vector<string> result = bigSorting(unsorted);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
