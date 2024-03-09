c625c1f4e863f91b696b082d3016ab5b07f0a97a
Ihor Vodko <ihorfinance@gmail.com>
2024-03-09 15:58:50 +0200

    Create FunnyStrings.cpp

---
+++ Algorithms/FunnyStrings.cpp
@@ -0,0 +1,75 @@
+// problem : https://www.hackerrank.com/challenges/funny-string/problem?isFullScreen=true
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'funnyString' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+// #include <string>
+// #include <cmath>
+
+std::string funnyString(std::string const & _str) {
+    auto fIt = _str.cbegin();
+    auto rIt = _str.crbegin();
+    while(fIt != _str.cend() - 1){
+        if(::abs(*fIt - *++fIt) != ::abs(*rIt - *++rIt)){
+            return "Not Funny";
+        }
+    }
+    return "Funny";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        string result = funnyString(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
