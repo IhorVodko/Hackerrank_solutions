3b5b8c24113c1b3a729c7ca7ac111209e5089644
Ihor Vodko <ihorfinance@gmail.com>
2024-03-12 19:31:45 +0200

    Create beautifulBinaryString.cpp

---
+++ Algorithms/beautifulBinaryString.cpp
@@ -0,0 +1,75 @@
+// problem : https://www.hackerrank.com/challenges/beautiful-binary-string/problem?isFullScreen=true
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'beautifulBinaryString' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING b as parameter.
+ */
+ 
+// #include <string>
+
+int beautifulBinaryString(std::string _s){
+    std::size_t minSteps = 0;
+    for(auto it = _s.cbegin(); it < _s.cend() - 2;){
+        if(*it++ == '0'){
+            if(*it == '1'){
+                if(*(++it)++ == '0'){
+                    ++minSteps;
+                }
+            }
+        }
+    }
+    return minSteps;   
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string b;
+    getline(cin, b);
+
+    int result = beautifulBinaryString(b);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
