3770f92911becaa680be801275e0e738b8009e9f
Ihor Vodko <ihorfinance@gmail.com>
2024-03-12 21:19:54 +0200

    Create closestNumbers.cpp

---
+++ Algorithms/closestNumbers.cpp
@@ -0,0 +1,112 @@
+// problem : https://www.hackerrank.com/challenges/closest-numbers/problem?isFullScreen=true
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'closestNumbers' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+// #include <algorithm>
+// #include <map>
+// #include <vector>
+// #include <utility>
+
+// #include <cstdlib>
+
+std::vector<int> closestNumbers(std::vector<int> _v) {
+    std::sort(_v.begin(), _v.end());
+    std::map<int, std::vector<int>> diff;
+    for(auto it = _v.cbegin(); it != _v.cend() - 1; ++it){
+        auto & vecTemp = diff[std::abs(*it - *(it + 1))];
+        vecTemp.insert(vecTemp.end(), it, it + 2);
+    }
+    return (*diff.begin()).second;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    vector<int> result = closestNumbers(arr);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << " ";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
