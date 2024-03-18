03418b3ed67644ef62f50971fa871fe702ec5f9d
Ihor Vodko <ihorfinance@gmail.com>
2024-03-18 18:17:22 +0200

    Create marcsCakewalk.cpp

---
+++ Algorithms/marcsCakewalk.cpp
@@ -0,0 +1,107 @@
+// problem :  https://www.hackerrank.com/challenges/marcs-cakewalk/problem?isFullScreen=true
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'marcsCakewalk' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts INTEGER_ARRAY calorie as parameter.
+ */
+
+// #include <vector>
+// #include <algorithm>
+
+// #include <cmath>
+
+long int marcsCakewalk(std::vector<int> _calories){
+    std::sort(
+        _calories.begin(), _calories.end(),
+        [&](auto const & _x, auto const & _y){
+            return _x > _y;
+        }
+    );
+    long int minMiles = 0;
+    size_t exp = 0;
+    for(auto const & cal : _calories){
+        minMiles += pow(2, exp++) * cal; 
+    }
+    return minMiles;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string calorie_temp_temp;
+    getline(cin, calorie_temp_temp);
+
+    vector<string> calorie_temp = split(rtrim(calorie_temp_temp));
+
+    vector<int> calorie(n);
+
+    for (int i = 0; i < n; i++) {
+        int calorie_item = stoi(calorie_temp[i]);
+
+        calorie[i] = calorie_item;
+    }
+
+    long result = marcsCakewalk(calorie);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
