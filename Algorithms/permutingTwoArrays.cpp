3c209fce7342b8393d494bc57926fa1168f59092
Ihor Vodko <ihorfinance@gmail.com>
2024-03-20 18:03:19 +0200

    Create permutingTwoArrays.cpp

---
+++ Algorithms/permutingTwoArrays.cpp
@@ -0,0 +1,134 @@
+// problem : https://www.hackerrank.com/challenges/two-arrays/problem?isFullScreen=true
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'twoArrays' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY A
+ *  3. INTEGER_ARRAY B
+ */
+
+// #include <algorithm>
+// #include <functional>
+// #include <string>
+// #include <vector>
+
+std::string twoArrays(
+        int _min
+    ,   std::vector<int> _v1
+    ,   std::vector<int> _v2
+){
+    std::sort(_v1.begin(), _v1.end());
+    std::sort(_v2.begin(), _v2.end(), std::greater<int>());
+    for(auto i = 0; i != _v1.size(); ++i){
+        if(_v1[i] + _v2[i] < _min){
+            return "NO";
+        }
+    }
+    return "YES";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string first_multiple_input_temp;
+        getline(cin, first_multiple_input_temp);
+
+        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+        int n = stoi(first_multiple_input[0]);
+
+        int k = stoi(first_multiple_input[1]);
+
+        string A_temp_temp;
+        getline(cin, A_temp_temp);
+
+        vector<string> A_temp = split(rtrim(A_temp_temp));
+
+        vector<int> A(n);
+
+        for (int i = 0; i < n; i++) {
+            int A_item = stoi(A_temp[i]);
+
+            A[i] = A_item;
+        }
+
+        string B_temp_temp;
+        getline(cin, B_temp_temp);
+
+        vector<string> B_temp = split(rtrim(B_temp_temp));
+
+        vector<int> B(n);
+
+        for (int i = 0; i < n; i++) {
+            int B_item = stoi(B_temp[i]);
+
+            B[i] = B_item;
+        }
+
+        string result = twoArrays(k, A, B);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
