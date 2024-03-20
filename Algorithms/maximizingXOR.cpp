3db42a5d7eb31ca07766130e33ff76843803a711
Ihor Vodko <ihorfinance@gmail.com>
2024-03-20 23:05:56 +0200

    Create maximizingXOR.cpp

---
+++ Algorithms/maximizingXOR.cpp
@@ -0,0 +1,76 @@
+// problem : https://www.hackerrank.com/challenges/maximizing-xor/problem?isFullScreen=true
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'maximizingXor' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER l
+ *  2. INTEGER r
+ */
+
+int maximizingXor(
+        int _min
+    ,   int _max
+){
+    size_t msbPosit = 0;
+    while(_min ^ _max){
+        ++msbPosit;
+        _min >>= 1;
+        _max >>= 1;
+    }
+    return (1 << msbPosit) - 1;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string l_temp;
+    getline(cin, l_temp);
+
+    int l = stoi(ltrim(rtrim(l_temp)));
+
+    string r_temp;
+    getline(cin, r_temp);
+
+    int r = stoi(ltrim(rtrim(r_temp)));
+
+    int result = maximizingXor(l, r);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
