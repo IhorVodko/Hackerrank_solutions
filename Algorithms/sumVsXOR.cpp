171a6b9b404f0614efd0863cbcfbc9ea195f97aa
Ihor Vodko <ihorfinance@gmail.com>
2024-03-28 19:03:23 +0200

    Create sumVsXOR.cpp

---
+++ Algorithms/sumVsXOR.cpp
@@ -0,0 +1,67 @@
+// problem: https://www.hackerrank.com/challenges/sum-vs-xor/problem?isFullScreen=true
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'sumXor' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER n as parameter.
+ */
+
+// #include <cmath>
+
+long int sumXor(long int _num) {
+    size_t satisfies = 0;
+    while(_num){
+        satisfies += _num % 2 == 0 ? 1 : 0;
+        _num /= 2;
+    }
+    return std::pow(2, satisfies);
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    long n = stol(ltrim(rtrim(n_temp)));
+
+    long result = sumXor(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
