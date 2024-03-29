0312dda84bbd512574443f2d4cd6e54432602fe3
Ihor Vodko <ihorfinance@gmail.com>
2024-03-29 17:56:03 +0200

    Create introToTutorialChallenges.cpp

---
+++ Algorithms/introToTutorialChallenges.cpp
@@ -0,0 +1,108 @@
+// problem: https://www.hackerrank.com/challenges/tutorial-intro/problem?isFullScreen=true
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'introTutorial' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER V
+ *  2. INTEGER_ARRAY arr
+ */
+
+// #include <algorithm>
+// #include <iterator>
+// #include <vector>
+
+int introTutorial(
+        int _num
+    ,   std::vector<int> const & _arr){
+    using namespace std;
+    return static_cast<int>(distance(   
+        cbegin(_arr),
+        find(cbegin(_arr), cend(_arr), _num)
+    ));
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string V_temp;
+    getline(cin, V_temp);
+
+    int V = stoi(ltrim(rtrim(V_temp)));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int result = introTutorial(V, arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
