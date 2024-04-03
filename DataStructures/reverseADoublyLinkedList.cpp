a6905f3a1cbf0f2f2ce6bc43868bbef93151f9e3
Ihor Vodko <ihorfinance@gmail.com>
2024-04-04 00:33:31 +0300

    Create reverseADoublyLinkedList.cpp

---
+++ DataStructures/reverseADoublyLinkedList.cpp
@@ -0,0 +1,156 @@
+// problem: https://www.hackerrank.com/challenges/reverse-a-doubly-linked-list/problem?isFullScreen=true
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+class DoublyLinkedListNode {
+    public:
+        int data;
+        DoublyLinkedListNode *next;
+        DoublyLinkedListNode *prev;
+
+        DoublyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+            this->prev = nullptr;
+        }
+};
+
+class DoublyLinkedList {
+    public:
+        DoublyLinkedListNode *head;
+        DoublyLinkedListNode *tail;
+
+        DoublyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            DoublyLinkedListNode* node = new DoublyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+                node->prev = this->tail;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_doubly_linked_list(DoublyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+/*
+ * Complete the 'reverse' function below.
+ *
+ * The function is expected to return an INTEGER_DOUBLY_LINKED_LIST.
+ * The function accepts INTEGER_DOUBLY_LINKED_LIST llist as parameter.
+ */
+
+/*
+ * For your reference:
+ *
+ * DoublyLinkedListNode {
+ *     int data;
+ *     DoublyLinkedListNode* next;
+ *     DoublyLinkedListNode* prev;
+ * };
+ *
+ */
+
+#include <cassert>
+
+DoublyLinkedListNode * reverse(
+    DoublyLinkedListNode * _list
+){
+    if(!_list || !_list->next){
+        return _list;
+    }
+    _list->prev = _list->next;
+    _list->next = nullptr;
+    _list = _list->prev;
+    auto tempNext = new DoublyLinkedListNode(-1);
+    do{
+        tempNext = _list->next;
+        _list->next = _list->prev;
+        _list->prev = tempNext;
+    }while(tempNext &&  (_list = tempNext));
+    assert(_list);
+    return _list;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        DoublyLinkedList* llist = new DoublyLinkedList();
+
+        string llist_count_temp;
+        getline(cin, llist_count_temp);
+
+        int llist_count = stoi(ltrim(rtrim(llist_count_temp)));
+
+        for (int i = 0; i < llist_count; i++) {
+            string llist_item_temp;
+            getline(cin, llist_item_temp);
+
+            int llist_item = stoi(ltrim(rtrim(llist_item_temp)));
+
+            llist->insert_node(llist_item);
+        }
+
+        DoublyLinkedListNode* llist1 = reverse(llist->head);
+
+        print_doubly_linked_list(llist1, " ", fout);
+        fout << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
