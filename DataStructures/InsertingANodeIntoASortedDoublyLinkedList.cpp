db2a1c542d7fc9fbc7668329a3c12c90c75aa1a3
Ihor Vodko <ihorfinance@gmail.com>
2024-04-04 00:15:35 +0300

    Create InsertingANodeIntoASortedDoublyLinkedList.cpp

---
+++ DataStructures/InsertingANodeIntoASortedDoublyLinkedList.cpp
@@ -0,0 +1,171 @@
+// problem: https://www.hackerrank.com/challenges/insert-a-node-into-a-sorted-doubly-linked-list/problem?isFullScreen=true
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+class DoublyLinkedListNode {
+    public:
+        int data;
+        DoublyLinkedListNode *next;
+        DoublyLinkedListNode *prev;
+
+        DoublyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+            this->prev = nullptr;
+        }
+};
+
+class DoublyLinkedList {
+    public:
+        DoublyLinkedListNode *head;
+        DoublyLinkedListNode *tail;
+
+        DoublyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            DoublyLinkedListNode* node = new DoublyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+                node->prev = this->tail;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_doubly_linked_list(DoublyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+/*
+ * Complete the 'sortedInsert' function below.
+ *
+ * The function is expected to return an INTEGER_DOUBLY_LINKED_LIST.
+ * The function accepts following parameters:
+ *  1. INTEGER_DOUBLY_LINKED_LIST llist
+ *  2. INTEGER data
+ */
+
+/*
+ * For your reference:
+ *
+ * DoublyLinkedListNode {
+ *     int data;
+ *     DoublyLinkedListNode* next;
+ *     DoublyLinkedListNode* prev;
+ * };
+ *
+ */
+
+#include <cassert>
+
+DoublyLinkedListNode * sortedInsert(
+        DoublyLinkedListNode * _list
+    ,   int _data
+){
+    assert(_list);
+    auto node = _list;
+    while(node->data < _data && node->next){
+        node = node->next;
+    }
+    auto nodeInserted = new DoublyLinkedListNode(_data);
+    if(node->data < _data){
+        node->next = nodeInserted;
+        nodeInserted->prev = node;
+        return _list;
+    }
+    if(node->prev){
+        node->prev->next = nodeInserted;
+        nodeInserted->prev = node->prev;
+        nodeInserted->next = node;
+        node->prev = nodeInserted;
+        return _list;
+    }
+    nodeInserted->next = node;
+    node->prev = nodeInserted;
+    return nodeInserted;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        DoublyLinkedList* llist = new DoublyLinkedList();
+
+        string llist_count_temp;
+        getline(cin, llist_count_temp);
+
+        int llist_count = stoi(ltrim(rtrim(llist_count_temp)));
+
+        for (int i = 0; i < llist_count; i++) {
+            string llist_item_temp;
+            getline(cin, llist_item_temp);
+
+            int llist_item = stoi(ltrim(rtrim(llist_item_temp)));
+
+            llist->insert_node(llist_item);
+        }
+
+        string data_temp;
+        getline(cin, data_temp);
+
+        int data = stoi(ltrim(rtrim(data_temp)));
+
+        DoublyLinkedListNode* llist1 = sortedInsert(llist->head, data);
+
+        print_doubly_linked_list(llist1, " ", fout);
+        fout << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
