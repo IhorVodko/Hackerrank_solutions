92fd685ef74b0c077ba2506e29a1d778d909e039
Ihor Vodko <ihorfinance@gmail.com>
2024-04-04 19:44:54 +0300

    Create treeLevelOrderTraversal.cpp

---
+++ DataStructures/treeLevelOrderTraversal.cpp
@@ -0,0 +1,89 @@
+// problem: https://www.hackerrank.com/challenges/tree-level-order-traversal/problem?isFullScreen=true
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+	
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+                }
+
+               return root;
+           }
+        }
+/*
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+*/
+
+    void levelOrder(Node * _root) {
+        using namespace std;
+        auto nodes = queue<decltype(_root)>({_root});
+        decltype(_root) node = nullptr;
+        while(!nodes.empty()){
+            node = nodes.front();
+            nodes.pop();
+            cout << node->data << " ";
+            if(node->left){
+                nodes.emplace(node->left);
+            }
+            if(node->right){
+                nodes.emplace(node->right);
+            }
+        }
+    }
+
+}; //End of Solution
+
+int main() {
+    
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+    myTree.levelOrder(root);
+
+    return 0;
+}
