28ec8a0f5230303aef4759c07c0f429779224dbc
Ihor Vodko <ihorfinance@gmail.com>
2024-04-04 16:17:19 +0300

    Create treePreorderTraversal.cpp

---
+++ DataStructures/treePreorderTraversal.cpp
@@ -0,0 +1,85 @@
+// problem: https://www.hackerrank.com/challenges/tree-preorder-traversal/problem?isFullScreen=true
+
+
+#include <iostream>
+#include <cstddef>
+	
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+                }
+
+               return root;
+           }
+        }
+
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+    void preOrder(Node * _root){
+        using namespace std;
+        if(!_root){
+            return;
+        }
+        cout << _root->data << " ";
+        preOrder(_root->left);
+        preOrder(_root->right);
+    }
+
+}; //End of Solution
+
+int main() {
+    
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+    myTree.preOrder(root);
+
+    return 0;
+}
