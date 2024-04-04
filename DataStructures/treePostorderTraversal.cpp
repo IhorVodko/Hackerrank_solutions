6b8f3a62e0f133538e9cff48b592d2892c630ff1
Ihor Vodko <ihorfinance@gmail.com>
2024-04-04 16:25:08 +0300

    Create treePostorderTraversal.cpp

---
+++ DataStructures/treePostorderTraversal.cpp
@@ -0,0 +1,86 @@
+// problem: https://www.hackerrank.com/challenges/tree-postorder-traversal/problem?isFullScreen=true
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+	
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+                }
+
+               return root;
+           }
+        }
+
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+
+    void postOrder(Node * _root){
+        if(!_root){
+            return; 
+        }
+        postOrder(_root->left);
+        postOrder(_root->right);
+        std::cout << _root->data << " ";
+    }
+
+}; //End of Solution
+
+int main() {
+    
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+    myTree.postOrder(root);
+
+    return 0;
+}
