e7c6031ba206286d93eed4554b64031462295b95
Ihor Vodko <ihorfinance@gmail.com>
2024-04-09 18:42:28 +0300

    Create equalStacks.cpp

---
+++ DataStructures/equalStacks.cpp
@@ -0,0 +1,149 @@
+// problem: https://www.hackerrank.com/challenges/equal-stacks/problem?isFullScreen=true
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'equalStacks' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY h1
+ *  2. INTEGER_ARRAY h2
+ *  3. INTEGER_ARRAY h3
+ */
+
+#include <numeric>
+#include <vector>
+
+int equalStacks(
+        std::vector<int> const & _h1
+    ,   std::vector<int> const & _h2
+    ,   std::vector<int> const & _h3
+){
+    using namespace std;
+    auto sum1 = accumulate(cbegin(_h1), cend(_h1), 0);
+    auto sum2 = accumulate(cbegin(_h2), cend(_h2), 0);
+    auto sum3 = accumulate(cbegin(_h3), cend(_h3), 0);
+    auto it1 = begin(_h1);
+    auto it2 = begin(_h2);
+    auto it3 = begin(_h3);
+    while(!(sum1 == sum2 && sum2 == sum3)){
+        if(sum1 >= sum2 && sum1 >= sum3){
+            sum1 -= *it1++;
+        } else if(sum2 >= sum3){
+            sum2 -= *it2++;
+        } else{
+            sum3 -= *it3++;
+        }
+    }
+    return sum1;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n1 = stoi(first_multiple_input[0]);
+
+    int n2 = stoi(first_multiple_input[1]);
+
+    int n3 = stoi(first_multiple_input[2]);
+
+    string h1_temp_temp;
+    getline(cin, h1_temp_temp);
+
+    vector<string> h1_temp = split(rtrim(h1_temp_temp));
+
+    vector<int> h1(n1);
+
+    for (int i = 0; i < n1; i++) {
+        int h1_item = stoi(h1_temp[i]);
+
+        h1[i] = h1_item;
+    }
+
+    string h2_temp_temp;
+    getline(cin, h2_temp_temp);
+
+    vector<string> h2_temp = split(rtrim(h2_temp_temp));
+
+    vector<int> h2(n2);
+
+    for (int i = 0; i < n2; i++) {
+        int h2_item = stoi(h2_temp[i]);
+
+        h2[i] = h2_item;
+    }
+
+    string h3_temp_temp;
+    getline(cin, h3_temp_temp);
+
+    vector<string> h3_temp = split(rtrim(h3_temp_temp));
+
+    vector<int> h3(n3);
+
+    for (int i = 0; i < n3; i++) {
+        int h3_item = stoi(h3_temp[i]);
+
+        h3[i] = h3_item;
+    }
+
+    int result = equalStacks(h1, h2, h3);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
